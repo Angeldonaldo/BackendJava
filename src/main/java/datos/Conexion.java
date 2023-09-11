d6f887e2ca75903cbc1ce72e5f26bd7239acc070
Donald <donalduomx@outlook.com>
2023-09-10 18:05:15 -0600

    v1.0.1 Conexion datasouce pool conection, se elimanan los archivos .class

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -2,6 +2,8 @@ package datos;
 
 import java.sql.Connection;
 import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
 
@@ -9,9 +11,19 @@ public class Conexion {
     private static final String USER = "root";
     private static final String PASSWORD = "admin";
 
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(URL);
+        ds.setUsername(USER);
+        ds.setPassword(PASSWORD);
+                
+         //tamanio inicial
+          ds.setInitialSize(5);
+          return ds;
+    }
     public static Connection getConnection()throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-
+        //return DriverManager.getConnection(URL, USER, PASSWORD);
+        return getDataSource().getConnection();
     }
 
     public static void close(ResultSet rs) throws SQLException {
